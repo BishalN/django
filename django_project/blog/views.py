f7989f634706efaf0c6cc93ddb59cddbf9056717
Bishal Neupane <neupanebishal07@gmail.com>
2021-01-09 08:15:56 +0545

    Database migration and populating data from the sqllite database

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
-
-posts = [
-    {
-        'author':"bishal neupane",
-        'title':"the best and fastest typer in history of human kind",
-        'content':"this is the fake information about bishal the great person of the world",
-        'date_posted':"august 2nd"
-    }, 
-    {
-        'author':'john doe',
-        'title':'the faku of the faker in the universe of the great people',
-        'content':'this is very beautiful day how is the world out there ',
-        "date_posted":"2nd May"
-    }
-]
 
 def home(request):
     context = {
-        'posts':posts,
+        'posts':Post.objects.all(),
     }
     return render(request,'blog/home.html',context)
 
